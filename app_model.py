e265871a6a726764dd3bb2f7ed1ba0ae767f5a05
Jon3J <jon181197@gmail.com>
2025-01-10 13:13:08 +0100

    primera prueba funcional en pythonanywhere

--- app_model.py
+++ app_model.py
@@ -1,23 +1,181 @@
-from flask import Flask, jsonify, request
-import os
-import pickle
-import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import Lasso
 from sklearn.metrics import mean_squared_error, mean_absolute_percentage_error
 import numpy as np
+from flask import Flask, request, jsonify
+import os
+import pickle
+from sklearn.model_selection import cross_val_score
+import pandas as pd
 
-# os.chdir(os.path.dirname(__file__))
+root_path = "/home/XpirituSanti/taller_despliegue_directo/"
 
 app = Flask(__name__)
 app.config['DEBUG'] = True
 
 # Enruta la landing page (endpoint /)
-
+@app.route('/',methods=['GET'])
+def hello():
+    return "Bienvenido a mi API del modelo advertising"
 
 # Enruta la funcion al endpoint /api/v1/predict
+@app.route('/api/v1/predict',methods=['GET'])
+def predict(): # Ligado al endpoint '/api/v1/predict', con el mÃ©todo GET
 
+    model = pickle.load(open(root_path + 'ad_model.pkl','rb'))
+    tv = request.args.get('tv', None)
+    radio = request.args.get('radio', None)
+    newspaper = request.args.get('newspaper', None)
+
+    print(tv,radio,newspaper)
+    print(type(tv))
+
+    if tv is None or radio is None or newspaper is None:
+        return "/Args empty, the data are not enough to predict"
+    else:
+        prediction = model.predict([[float(tv),float(radio),float(newspaper)]])
+
+    return jsonify({'predictions': prediction[0]})
 
 # Enruta la funcion al endpoint /api/v1/retrain
+@app.route('/api/v1/retrain',methods=['GET'])
+def retrain(): # Rutarlo al endpoint '/api/v1/retrain/', metodo GET
+    if os.path.exists(root_path + "data/Advertising_new.csv"):
+        data = pd.read_csv(root_path + 'data/Advertising_new.csv')
 
-app.run()
+        X_train, X_test, y_train, y_test = train_test_split(data.drop(columns=['sales']),
+                                                        data['sales'],
+                                                        test_size = 0.20,
+                                                        random_state=42)
+
+        model = Lasso(alpha=6000)
+        model.fit(X_train, y_train)
+        rmse = np.sqrt(mean_squared_error(y_test, model.predict(X_test)))
+        mape = mean_absolute_percentage_error(y_test, model.predict(X_test))
+        model.fit(data.drop(columns=['sales']), data['sales'])
+        pickle.dump(model, open(root_path + 'ad_model.pkl', 'wb'))
+
+        return f"Model retrained. New evaluation metric RMSE: {str(rmse)}, MAPE: {str(mape)}"
+    else:
+        return f"<h2>New data for retrain NOT FOUND. Nothing done!</h2>"
+
+if __name__ == "__main__":
+    app.run()
+    
+    
+# This file contains the WSGI configuration required to serve up your
+# web application at http://XpirituSanti.pythonanywhere.com/
+# It works by setting the variable 'application' to a WSGI handler of some
+# description.
+#
+
+# +++++++++++ GENERAL DEBUGGING TIPS +++++++++++
+# getting imports and sys.path right can be fiddly!
+# We've tried to collect some general tips here:
+# https://help.pythonanywhere.com/pages/DebuggingImportError
+
+
+# +++++++++++ HELLO WORLD +++++++++++
+# A little pure-wsgi hello world we've cooked up, just
+# to prove everything works.  You should delete this
+# code to get your own working.
+
+
+HELLO_WORLD = """<html>
+<head>
+    <title>PythonAnywhere hosted web application</title>
+</head>
+<body>
+<h1>Hello, World!</h1>
+<p>
+    This is the default welcome page for a
+    <a href="https://www.pythonanywhere.com/">PythonAnywhere</a>
+    hosted web application.
+</p>
+<p>
+    Find out more about how to configure your own web application
+    by visiting the <a href="https://www.pythonanywhere.com/web_app_setup/">web app setup</a> page
+</p>
+</body>
+</html>"""
+
+
+def application(environ, start_response):
+    if environ.get('PATH_INFO') == '/':
+        status = '200 OK'
+        content = HELLO_WORLD
+    else:
+        status = '404 NOT FOUND'
+        content = 'Page not found.'
+    response_headers = [('Content-Type', 'text/html'), ('Content-Length', str(len(content)))]
+    start_response(status, response_headers)
+    yield content.encode('utf8')
+
+
+# Below are templates for Django and Flask.  You should update the file
+# appropriately for the web framework you're using, and then
+# click the 'Reload /yourdomain.com/' button on the 'Web' tab to make your site
+# live.
+
+# +++++++++++ VIRTUALENV +++++++++++
+# If you want to use a virtualenv, set its path on the web app setup tab.
+# Then come back here and import your application object as per the
+# instructions below
+
+
+# +++++++++++ CUSTOM WSGI +++++++++++
+# If you have a WSGI file that you want to serve using PythonAnywhere, perhaps
+# in your home directory under version control, then use something like this:
+#
+#import sys
+#
+#path = '/home/XpirituSanti/path/to/my/app
+#if path not in sys.path:
+#    sys.path.append(path)
+#
+#from my_wsgi_file import application  # noqa
+
+
+# +++++++++++ DJANGO +++++++++++
+# To use your own django app use code like this:
+#import os
+#import sys
+#
+## assuming your django settings file is at '/home/XpirituSanti/mysite/mysite/settings.py'
+## and your manage.py is is at '/home/XpirituSanti/mysite/manage.py'
+#path = '/home/XpirituSanti/mysite'
+#if path not in sys.path:
+#    sys.path.append(path)
+#
+#os.environ['DJANGO_SETTINGS_MODULE'] = 'mysite.settings'
+#
+## then:
+#from django.core.wsgi import get_wsgi_application
+#application = get_wsgi_application()
+
+
+
+# +++++++++++ FLASK +++++++++++
+# Flask works like any other WSGI-compatible framework, we just need
+# to import the application.  Often Flask apps are called "app" so we
+# may need to rename it during the import:
+#
+#
+import sys
+#
+## The "/home/XpirituSanti" below specifies your home
+## directory -- the rest should be the directory you uploaded your Flask
+## code to underneath the home directory.  So if you just ran
+## "git clone git@github.com/myusername/myproject.git"
+## ...or uploaded files to the directory "myproject", then you should
+## specify "/home/XpirituSanti/myproject"
+path = '/home/XpirituSanti/taller_despliegue_directo'
+if path not in sys.path:
+    sys.path.append(path)
+#
+from app_model import app as application  # noqa
+#
+# NB -- many Flask guides suggest you use a file called run.py; that's
+# not necessary on PythonAnywhere.  And you should make sure your code
+# does *not* invoke the flask development server with app.run(), as it
+# will prevent your wsgi file from working.
